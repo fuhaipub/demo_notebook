93005c3d3910323f0884ed70a9f68ab7c16c26c3
fuhaipub@qq.com <fuhaipub@qq.com>
2022-09-23 12:27:55 +0000

    add a nb2

---
+++ demo_nb2.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+for i in range(10):
+    print("hello")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # This is a demo 
+# MAGIC ##test github commit
+
+# COMMAND ----------
+
+
